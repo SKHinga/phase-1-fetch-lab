a7563d62e55e47fa247b00a415f633cea256dc63
Samuel Hinga <samuelhinga200@gmail.com>
2022-04-25 08:35:29 +0300

    commit tree cb34b4d6e7eb68bcd41e59aab89c1b6afbaf2aa3

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(json => {
+    renderBooks(json)
+    getTotalPages(json)
+    console.log(json[4])
+  })
   
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const head = document.createElement('head');
+    head.innerHTML = book.name;
+    main.appendChild(head);
   });
+}
+
+function getTotalPages(books) {
+  let totalPages = 0
+  books.forEach(book => {
+    totalPages += book["numberOfPages"]
+  });
+  console.log(`The total pages is ${totalPages}`)
+  console.log(`The fifth book in this series is ${books[4]["name"]}`)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
